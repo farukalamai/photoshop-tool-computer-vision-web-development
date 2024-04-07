a528c7e9e1b016064dc2efc7db5188bd0c304550
farukt <mdfarukt222@gmail.com>
2024-04-07 23:59:41 +0600

    project completed

--- app/main.js
+++ app/main.js
@@ -5,5 +5,48 @@ function openCvReady() {
         let imgMain = cv.imread("img-main");
         cv.imshow("main-canvas", imgMain);
         imgMain.delete();
-    }
+
+        // RgB button
+        document.getElementById("button-rgb").onclick = function () {
+            let imgMain = cv.imread("img-main");
+            cv.imshow("main-canvas", imgMain);
+            imgMain.delete();
+         
+        };
+         // Gray button
+        document.getElementById("button-gray").onclick = function () {
+            let imgMain = cv.imread("img-main");
+            let imgGray = imgMain.clone();
+            // converting image to gray scale
+            cv.cvtColor(imgMain, imgGray, cv.COLOR_RGBA2GRAY, 0);
+            cv.imshow("main-canvas", imgGray);
+            imgMain.delete();
+            imgGray.delete();
+         
+        };
+        // Blur button
+        document.getElementById("button-blur").onclick = function () {
+            let imgMain = cv.imread("img-main");
+            let imgBlur = imgMain.clone();
+            let ksize = new cv.Size(49, 49);
+            cv.GaussianBlur(imgMain, imgBlur, ksize, 0);
+            cv.imshow("main-canvas", imgBlur);
+            imgMain.delete();
+            imgBlur.delete();
+         
+        };
+        // Edge button
+        document.getElementById("button-edge").onclick = function () {
+            let imgMain = cv.imread("img-main");
+            let imgCanny = imgMain.clone();
+            cv.Canny(imgMain, imgCanny, 50, 100);
+            cv.imshow("main-canvas", imgCanny);
+            imgMain.delete();
+            imgCanny.delete();
+         
+        };
+
+
+
+    };
 }
